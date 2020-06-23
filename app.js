26b756719745170578c50a1ab239510746aea9f1
catalogue-taba <67260595+catalogue-taba@users.noreply.github.com>
2020-06-23 08:48:16 +0200

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ console.log('ouverture');
 if ( 'serviceWorker' in navigator ) {  
   window.addEventListener ( 'load' , () => {  
 console.log('app java ok');  
-    navigator.serviceWorker.register ( 'sw.js', { scope: '/cataloguetaba/' }); 
+    navigator.serviceWorker.register ( 'sw.js', { scope: '/' }); 
 	console.log('service worker en route');
   });
 }
