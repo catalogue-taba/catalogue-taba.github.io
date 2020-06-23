c03ab49e506ba626679ecc286d6776d72cb506ec
catalogue-taba <67260595+catalogue-taba@users.noreply.github.com>
2020-06-23 09:42:47 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -33,25 +33,21 @@ self.addEventListener('install', function(event) {
 		});
 self.addEventListener('fetch', function(event) {
 	console.log('fonction event');
-	event.respondWith(caches.match(event.request).then(function(response) {
-		console.log('reponse reseau')
-		// caches.match() always resolves
-		// but in case of success response will have value
-		if (response !== undefined) {
-			return response;
-			} else {
+	event.respondWith(
+		caches.match(event.request).then(function(response) {
+		console.log('reponse reseau'+event.request.url)
+	
 				return fetch(event.request).then(function (response) {
 					// response may be used only once
 					// we need to save clone to put one copy in cache
 					// and serve second one
-					let responseClone = response.clone();
-					caches.open('test_manifest-v7').then(function (cache) {
+					
+					return caches.open('test_manifest-v7').then(function (cache) {
 						cache.put(event.request, responseClone);
 						});
 						return response;
-						}).catch(function () {
-							return caches.match('croix.jpg');
-							});
-							}
-							}));
-							});
+						});
+      });
+    })
+  );
+});
